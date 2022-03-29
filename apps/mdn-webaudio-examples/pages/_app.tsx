4c31835cf6a29652412b7643b408482433899b40
Evan Harmon <evan.p.harmon@gmail.com>
2022-03-29 13:08:10 -0400

    add audiobasics css to global _app.tsx. This will affect other examples...

--- apps/mdn-webaudio-examples/pages/_app.tsx
+++ apps/mdn-webaudio-examples/pages/_app.tsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import './index.css'
+import './audiobasics.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
--- apps/mdn-webaudio-examples/pages/audiobasics/AudioBasics.tsx
+++ apps/mdn-webaudio-examples/pages/audiobasics/AudioBasics.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import styles from './AudioBasics.module.css'
 import { ChangeEvent } from 'react'
 
 let audioCtx: AudioContext
