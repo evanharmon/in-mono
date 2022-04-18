d700ee7b8b0fe6a8c371aeef1f16dfab7562e940
Evan Harmon <evan.p.harmon@gmail.com>
2022-04-18 16:52:55 -0400

    adjust mdn-webaudio-examples to support dynamic transpile-modules in next.config.js

--- apps/mdn-webaudio-examples/next.config.js
+++ apps/mdn-webaudio-examples/next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const pkg = require('./package.json')
+
+/* BEGIN: grab '*' turbo package dependencies to track */
+const filterEntries = (obj, filterVal) =>
+  Object.entries(obj)
+    .filter(([k, v]) => typeof k === 'string' && typeof v === 'string')
+    .map(([k, v]) => {
+      if (v === filterVal) return k
+    })
+    .filter(i => typeof i !== 'undefined')
+
+const turboDeps = filterEntries(pkg.dependencies || {}, '*')
+const turboDevDeps = filterEntries(pkg.devDependencies || {}, '*')
+
+const withTM = require('next-transpile-modules')([
+  ...turboDeps,
+  ...turboDevDeps,
+])
+/* END: grab '*' turbo package dependencies to track */
+
+const nextConfig = withTM({
   reactStrictMode: true,
-}
+})
 
 module.exports = nextConfig
