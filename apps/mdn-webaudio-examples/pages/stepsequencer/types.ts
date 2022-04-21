512da22e50b6c8e279bf9525656425a32a637f09
Evan Harmon <evan.p.harmon@gmail.com>
2022-04-20 22:03:12 -0400

    stepsequencer: working timing, css and animation. no sound or pad clicks implemented yet

--- apps/mdn-webaudio-examples/pages/stepsequencer/types.ts
+++ apps/mdn-webaudio-examples/pages/stepsequencer/types.ts
@@ -1,33 +1,25 @@
 // NOTE: yes all the pad types could be made more generic
 // I am being explicit here as it's a learning exercise
 export interface playSweepParams {
-  ctx: AudioContext
   attackTime: number
   releaseTime: number
   wave: any
-  time: number
   sweepLength: number
 }
 
 export interface playPulseParams {
-  ctx: AudioContext
   pulseHz: number
   pulseTime: number
   lfoHz: number
-  time: number
 }
 
 export interface playNoiseParams {
-  ctx: AudioContext
-  time: number
   noiseDuration: number
   bandHz: number
 }
 
 export interface playSampleParams {
-  ctx: AudioContext
   audioBuffer: AudioBuffer
-  time: number
   playbackRate: number
 }
 
@@ -37,18 +29,9 @@ export interface NoteQueueProps {
 }
 
 export interface schedulerParams {
-  ctx: AudioContext
-  noteBeatNumber: number
-  time: number
   sweepGrid: Map<number, Object>
   pulseGrid: Map<number, Object>
   noiseGrid: Map<number, Object>
   sampleGrid: Map<number, Object>
-  notesInQueue: Array<NoteQueueProps>
-  currentNote: number
-  nextNoteTime: number
-  scheduleAheadTime: number
-  lookAhead: number
-  timerId: number
   audioBuffer?: AudioBuffer
 }
