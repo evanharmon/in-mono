dec70fbe0911d6558f16ec571511fcd8b5d5d52a
Evan Harmon <evan.p.harmon@gmail.com>
2022-05-11 09:35:37 -0400

    add utils package with AudioContext window setup

--- apps/mdn-webaudio-examples/components/CompressorExample/CompressorExample.tsx
+++ apps/mdn-webaudio-examples/components/CompressorExample/CompressorExample.tsx
@@ -1,3 +1,32 @@
+import { useState } from 'react'
+import { AudioContext } from '@evanharmon/utils'
+
+let audioContext: AudioContext | null = null
+if (audioContext !== null && AudioContext !== null) {
+  audioContext = new AudioContext()
+}
+
+async function resumeAudio() {
+  if (audioContext?.state === 'suspended') {
+    await audioContext.resume()
+  }
+
+  return
+}
+
 export default function CompressorExample() {
-  return <></>
+  const [isActive, setIsActive] = useState(false)
+
+  const onAddCompressionClickHandler = async () => {
+    await resumeAudio()
+  }
+
+  return <>
+    <h1>Compressor example</h1>
+    <audio controls>
+      <source src="viper.ogg" type="audio/ogg" />
+      <source src="viper.mp3" type="audio/mp3" />
+    </audio>
+    <button data-active={isActive} onClick={onAddCompressionClickHandler}>Add compression</button>
+  </>
 }
