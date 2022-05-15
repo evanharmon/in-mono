024819eb57a826fa81b154063e8560b94346e806
Evan Harmon <evan.p.harmon@gmail.com>
2022-05-15 17:30:50 -0400

    WIP compressor example

--- apps/mdn-webaudio-examples/components/CompressorExample/CompressorExample.tsx
+++ apps/mdn-webaudio-examples/components/CompressorExample/CompressorExample.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { AudioContext } from '@evanharmon/utils'
 
 let audioContext: AudioContext | null = null
@@ -6,27 +6,49 @@ if (audioContext !== null && AudioContext !== null) {
   audioContext = new AudioContext()
 }
 
-async function resumeAudio() {
-  if (audioContext?.state === 'suspended') {
-    await audioContext.resume()
-  }
-
-  return
-}
-
 export default function CompressorExample() {
   const [isActive, setIsActive] = useState(false)
+  const audioRef = useRef<HTMLMediaElement | null>(null)
+
+  useEffect(() => {
+    function onPlay() {
+      console.log(`play called`, audioRef)
+      // TODO get this working
+      if (!audioContext || !audioRef.current) return
+
+      const source = audioContext?.createMediaElementSource(audioRef.current)
+
+      const compressor = audioContext?.createDynamicsCompressor()
+      if (!compressor) return
+
+      compressor.threshold.value = -50
+      compressor.knee.value = 40
+      compressor.ratio.value = 12
+      compressor.attack.value = 0
+      compressor.release.value = 0.25
+
+      source.connect(audioContext?.destination)
+    }
+
+    if (audioRef && audioRef.current) {
+      audioRef.current.addEventListener('play', onPlay)
+    }
+
+    return () => {
+      if (audioRef && audioRef.current) {
+        audioRef.current.removeEventListener('play', onPlay)
+      }
+    }
+  }, [])
 
   const onAddCompressionClickHandler = async () => {
-    await resumeAudio()
   }
 
   return <>
     <h1>Compressor example</h1>
-    <audio controls>
-      <source src="viper.ogg" type="audio/ogg" />
-      <source src="viper.mp3" type="audio/mp3" />
-    </audio>
-    <button data-active={isActive} onClick={onAddCompressionClickHandler}>Add compression</button>
+    <audio ref={audioRef} controls src="viper.mp3" />
+    <button data-active={isActive} onClick={onAddCompressionClickHandler}>
+      {isActive ? 'Remove compression' : 'Add compression'}
+    </button>
   </>
 }
