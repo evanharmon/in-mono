5926df22cf347ddc559c022021792f741785733a
Evan Harmon <evan.p.harmon@gmail.com>
2022-05-19 12:06:00 -0400

    13 auth0 repo setup (#17)
    * music app: add config packages and install next-transpile-modules
    
    * add auth0 .env.development values for music app
    
    add @auth0/nextjs-auth0 package to music app for api
    
    * add auth0 note with node cli command to create hex secret for cookie
    
    * fix issuer base url in music
    
    * functional working login logout with auth0 nextjs package
    
    * add template for music .env.development.local

--- apps/music/pages/_app.tsx
+++ apps/music/pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { UserProvider } from '@auth0/nextjs-auth0'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // support passing user prop to prepopulate useHook
+  const { user } = pageProps
+
+  return (
+    <UserProvider user={user}>
+      <Component {...pageProps} />
+    </UserProvider>
+  )
 }
 
 export default MyApp
