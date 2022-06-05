ebea0dd57120d08b91d97ef71c56b5639814bce2
Evan Harmon <evan.p.harmon@gmail.com>
2022-06-05 10:07:50 -0400

    do not render dropdown until user object available

--- apps/music/components/DropdownMenu/DropdownMenu.tsx
+++ apps/music/components/DropdownMenu/DropdownMenu.tsx
@@ -1,3 +1,4 @@
+import { useUser } from '@auth0/nextjs-auth0'
 import { ReactNode, useEffect, useRef, useState } from 'react'
 import { CSSTransition } from 'react-transition-group'
 import styled from 'styled-components'
@@ -9,6 +10,10 @@ const StyledLogoutButton = styled.a``
 interface DropdownItemProps {}
 
 export function DropdownMenu() {
+  const { user } = useUser()
+
+  if (!user) return <></>
+
   return (
     <>
       <StyledLogoutButton href='/api/auth/logout'>Logout</StyledLogoutButton>
