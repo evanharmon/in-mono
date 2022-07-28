7866d7b58828fd7f5b36f379612d822be755bac5
Evan Harmon <evan.p.harmon@gmail.com>
2022-07-28 15:19:57 +0000

    exercism typescript twofer challenge

---
+++ challenges/exercism/typescript/two-fer/two-fer.ts
@@ -0,0 +1,9 @@
+/**
+ * This stub is provided to make it straightforward to get started.
+ */
+
+export function twoFer(name = ''): string {
+  if (!name) return 'One for you, one for me.'
+
+  return `One for ${name}, one for me.`
+}
