8119aca58a6dc4e4887810ccd18651c1e7d58ee8
Evan Harmon <evan.p.harmon@gmail.com>
2022-08-10 18:40:24 -0400

    42 home page s3 ci cd (#43)
    * remove unnecessary app auto generated READMEs
    
    * adjust apps/music port to allow homepage to use 3000
    
    * add basic nextjs homepage app
    
    * add updated package-lock
    
    * add github workflow for deploying homepage to s3

---
+++ apps/home/pages/day01/index.tsx
@@ -0,0 +1,38 @@
+import styles from './day01.module.css'
+
+function Day01() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.introContent}>
+        <h1>Lorem ipsum dolor sit.</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod unde
+          rerum, deleniti ea obcaecati sint hic odit dicta tenetur qui ut
+          dolorum provident sit, atque, reprehenderit nulla voluptate! Officiis,
+          consectetur?
+        </p>
+        {/* comment out from here down to match re-sizing shown in video challenge */}
+        {/* <p>
+          Iste ipsa enim delectus porro, ullam repellendus maiores quis rem
+          debitis cum, necessitatibus architecto dolor? Velit, ad quaerat
+          blanditiis veritatis expedita totam vel voluptatem officiis officia ab
+          modi voluptatibus obcaecati.
+        </p>
+        <p>
+          Accusantium minima iusto nobis fuga hic explicabo unde illum,
+          perferendis et animi aperiam quaerat, eaque deleniti alias blanditiis
+          exercitationem commodi repudiandae ullam consequatur incidunt
+          reiciendis repellat officia laboriosam. Esse, modi.
+        </p>
+        <p>
+          Expedita cupiditate iure odit, delectus placeat optio magnam assumenda
+          mollitia aspernatur at saepe nisi commodi natus excepturi voluptate.
+          Recusandae nisi dolorem, necessitatibus optio aliquam repellat.
+          Adipisci, incidunt. Consequuntur, natus nulla.
+        </p> */}
+      </div>
+    </div>
+  )
+}
+
+export default Day01
--- apps/music/README.md
+++
@@ -1,3 +0,0 @@
-## Getting Started
-
-This app is designed to run in a turborepo. Refer to [the main README](../../README.md) for running the app
