fbaf8e4a345ac0830b6921068b0a097f9824548e
Evan Harmon <evan.p.harmon@gmail.com>
2022-08-17 22:58:25 +0000

    weather dashboard add ability to add zip

--- apps/react-exercises/pages/weather-dashboard/index.tsx
+++ apps/react-exercises/pages/weather-dashboard/index.tsx
@@ -1,19 +1,35 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 
 /*
   Step 1 Basic UI
     - [x] show zip codes
-    - [ ] user can add a zip code
+    - [x] user can add a zip code
     - [ ] user can remove a zip code
 */
 
 function WeatherDashboard() {
   const [zipcodes, setZipcodes] = useState<Array<string>>(['29464', '12112'])
+  const [inputVal, setInputVal] = useState<string>('')
+
+  function onChangeHandler(e: ChangeEvent<HTMLInputElement>) {
+    setInputVal(e.target.value)
+  }
+
+  function addZip() {
+    // avoid adding dupes
+    if (zipcodes.find(i => i === inputVal)) return
+
+    setZipcodes(prevState => [...prevState, inputVal])
+  }
 
   return (
     <>
       <div>
         <h1>Weather Dashboard</h1>
+      </div>
+      <div>
+        <button onClick={addZip}>add</button>
+        <input type='text' value={inputVal} onChange={onChangeHandler} />
       </div>
       <div>
         <ul>
