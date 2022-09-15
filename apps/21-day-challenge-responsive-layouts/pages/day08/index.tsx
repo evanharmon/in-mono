be0e598c82fca7acb7c9087311fd009064b785ee
Evan Harmon <evan.p.harmon@gmail.com>
2022-09-15 20:09:22 +0000

    day8 solution for 21 day challenge responsive layout

--- apps/21-day-challenge-responsive-layouts/pages/day08/index.tsx
+++ apps/21-day-challenge-responsive-layouts/pages/day08/index.tsx
@@ -1,7 +1,79 @@
 import styles from './day08.module.css'
 
 function Day08() {
-  return <></>
+  return (
+    <>
+      <div className={styles.hero}>
+        <div className={styles.container}>
+          <div className={styles.heroText}>
+            <h1>Responsive layouts don&apos;t have to be a struggle</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam.
+            </p>
+            <a href='#' className={styles.btn}>
+              I want to learn
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <section className={styles.threeCol}>
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <div className={styles.col}>
+              <h2>Cheap</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam.
+              </p>
+            </div>
+            <div className={styles.col}>
+              <h2>Quick</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam.
+              </p>
+            </div>
+            <div className={styles.col}>
+              <h2>Affordable</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.twoCol}>
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <div className={styles.col}>
+              <h2>Cheap</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam.
+              </p>
+            </div>
+            <div className={styles.col}>
+              <h2>Quick</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default Day08
