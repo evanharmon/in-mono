8ecfeea937642faa6485bb59b570b9ef7935e067
Evan Harmon <evan.p.harmon@gmail.com>
2022-12-15 10:37:51 -0500

    add mirror body to brooks_build routing rust example

--- courses/brooks_builds/routing/src/routes/mod.rs
+++ courses/brooks_builds/routing/src/routes/mod.rs
@@ -1,8 +1,15 @@
 mod hello_world;
+mod mirror_body_string;
 
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
 use hello_world::hello_world;
+use mirror_body_string::mirror_body_string;
 
 pub fn create_routes() -> Router {
-    Router::new().route("/", get(hello_world))
+    Router::new()
+        .route("/", get(hello_world))
+        .route("/mirror_body_string", post(mirror_body_string))
 }
