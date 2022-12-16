9af99d99cf3e86f059b019489dbb78b66afa1b85
Evan Harmon <evan.p.harmon@gmail.com>
2022-12-16 12:17:00 -0500

    brooks_build rust routing examples of mirroring custom header and user agent

--- courses/brooks_builds/routing/src/routes/mod.rs
+++ courses/brooks_builds/routing/src/routes/mod.rs
@@ -1,6 +1,8 @@
 mod hello_world;
 mod mirror_body_json;
 mod mirror_body_string;
+mod mirror_custom_header;
+mod mirror_user_agent;
 mod path_variables;
 mod query_params;
 
@@ -11,6 +13,8 @@ use axum::{
 use hello_world::hello_world;
 use mirror_body_json::mirror_body_json;
 use mirror_body_string::mirror_body_string;
+use mirror_custom_header::mirror_custom_header;
+use mirror_user_agent::mirror_user_agent;
 use path_variables::hard_coded_path;
 use path_variables::path_variables;
 use query_params::query_params;
@@ -24,4 +28,6 @@ pub fn create_routes() -> Router {
         .route("/path_variables/:id", get(path_variables))
         .route("/path_variables/15", get(hard_coded_path))
         .route("/query_params", get(query_params))
+        .route("/mirror_user_agent", get(mirror_user_agent))
+        .route("/mirror_custom_header", get(mirror_custom_header))
 }
