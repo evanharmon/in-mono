cd3e4bf52f0b0387c256ca8f3655e6664f70e1f5
Evan Harmon <evan.p.harmon@gmail.com>
2022-12-15 20:39:17 -0500

    brooks_build routing rust: add examples of path variables and query params

--- courses/brooks_builds/routing/src/routes/mod.rs
+++ courses/brooks_builds/routing/src/routes/mod.rs
@@ -1,15 +1,27 @@
 mod hello_world;
+mod mirror_body_json;
 mod mirror_body_string;
+mod path_variables;
+mod query_params;
 
 use axum::{
     routing::{get, post},
     Router,
 };
 use hello_world::hello_world;
+use mirror_body_json::mirror_body_json;
 use mirror_body_string::mirror_body_string;
+use path_variables::hard_coded_path;
+use path_variables::path_variables;
+use query_params::query_params;
 
+// ordering doesn't matter - most exact route match wins
 pub fn create_routes() -> Router {
     Router::new()
         .route("/", get(hello_world))
         .route("/mirror_body_string", post(mirror_body_string))
+        .route("/mirror_body_json", post(mirror_body_json))
+        .route("/path_variables/:id", get(path_variables))
+        .route("/path_variables/15", get(hard_coded_path))
+        .route("/query_params", get(query_params))
 }
