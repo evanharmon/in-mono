9899bf9d837273b8d4e7878faeb79e53ee22257c
Evan Harmon <evan.p.harmon@gmail.com>
2025-03-03 13:49:54 -0500

    simple CORS localhost for axum_api playground

--- rust_playgrounds/axum_api/README.md
+++ rust_playgrounds/axum_api/README.md
@@ -5,8 +5,22 @@
 ### Build image
 `docker build -t evanharmon/axum_api .`
 
+### Run locally
+`cargo run -- --allow-hostname localhost`
+
 ### Run container
-`docker run -i --name axum_api -p 3000:3000 --rm evanharmon/axum_api`
+```bash
+docker run -i --name axum_api -p 3000:3000 --rm evanharmon/axum_api axum_api \
+  --allow-hostname localhost
+```
+
+### Run compose
+```bash
+# Build and watch
+docker-compose up --build --watch
+# Teardown
+docker-compose down
+```
 
 ### Curl api
 `curl localhost:3000/`
--- rust_playgrounds/axum_api/src/main.rs
+++ rust_playgrounds/axum_api/src/main.rs
@@ -1,12 +1,20 @@
-use axum::{Router, routing::get};
+use axum::{
+    http::{header, HeaderValue, Method},
+    routing::get,
+    Router,
+};
 use clap::Parser;
 use log::info;
+use tower_http::cors::CorsLayer;
 
 /// CLI arguments for running server
 #[derive(Parser)]
 struct CliArgs {
+    /// CORS allowed hostname
+    #[arg(long)]
+    allow_hostname: String,
     /// port
-    #[arg(default_value_t = 3000)]
+    #[arg(long, default_value_t = 3000)]
     port: u16,
 }
 
@@ -16,8 +24,22 @@ async fn main() {
     env_logger::init();
     let args = CliArgs::parse();
 
+    // Cross-origin resource sharing (CORS)
+    // only setup for local playgrounds for now - no https
+    let origins = vec![
+        format!("http://{}:{}", args.allow_hostname, args.port)
+            .parse::<HeaderValue>()
+            .unwrap(),
+    ];
+    let cors = CorsLayer::new()
+        .allow_origin(origins)
+        .allow_methods(vec![Method::GET, Method::OPTIONS])
+        .allow_headers(vec![header::CONTENT_TYPE]);
+
     // app routing
-    let app = Router::new().route("/", get(|| async { "Success" }));
+    let app = Router::new()
+        .layer(cors)
+        .route("/", get(|| async { "Success" }));
 
     // startup server
     let bind_addr = &format!("0.0.0.0:{}", args.port);
