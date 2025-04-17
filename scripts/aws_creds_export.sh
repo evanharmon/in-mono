2c41a0c8791b213d404e0e2bd8c8f2f9eb692258
Evan Harmon <evan.p.harmon@gmail.com>
2025-04-17 18:38:53 -0400

    restore shell options in aws_creds_export script

--- scripts/aws_creds_export.sh
+++ scripts/aws_creds_export.sh
@@ -3,8 +3,6 @@
 # Usage: source aws_creds_export.sh profile_name
 # Description: exports the AWS credential variables to the shell
 
-# Don't use -e as this shouldn't close terminal on error
-set -uo pipefail
 
 BLUE="\033[94m"
 RED="\033[91m"
@@ -13,21 +11,33 @@ NC="\033[0m"
 profile="${1:?Missing aws profile name}"
 
 function export_creds {
+  # Save current shell options
+  local old_options
+  old_options=$(set +o)
+
+  # Strict options for this function
+  # Don't use -e as this shouldn't close terminal on error
+  set -uo pipefail
+
   output=$(aws configure export-credentials --profile "$profile" --format env-no-export)
   rv=$?
   if [ $rv -ne 0 ]; then
     echo -e "$RED error exporting credentials with aws cli $NC"
+    # Restore original shell options
+    eval "$old_options"
     return
   fi
 
-for line in $output; do
+  for line in $output; do
     # Disabling as `export "${line?}"` did not export correctly causing errors
-    # shellcheck disable=SC2116
-    # shellcheck disable=SC2046
+    # shellcheck disable=SC2116,SC2046
     export $(echo "$line")
   done
 
   echo -e "$BLUE Exported AWS credential environment variables $NC"
+
+  # Restore original shell options
+  eval "$old_options"
 }
 
 export_creds
